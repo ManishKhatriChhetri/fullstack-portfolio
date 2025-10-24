fedaf335ca26354e9de3f74d6c9ba8cf42597feb
Manish Khatri Chhetri <manish.khatrichhetri@my.utsa.edu>
2025-10-24 09:11:27 -0500

    feat: Add controller for project model

---
+++ backend/controllers/projectController.js
@@ -0,0 +1,137 @@
+const Project = require('../models/Project')
+
+exports.getAllProjects = async (req, res) => {
+    try {
+        const projects = await Project.find().sort({order: 1});
+        
+        res.status(200).json({
+            success: true,
+            count: projects.length,
+            data: projects
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to fetch projects',
+            error: error.message
+        });
+    }
+}
+
+exports.getFeaturedProjects = async (req, res) => {
+    try {
+        const projects = await Project.find({featured: true}).sort({order: 1});
+        
+        res.status(200).json({
+            success: true,
+            count: projects.length,
+            data: projects
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to fetch featured projects',
+            error: error.message
+        });
+    }
+}
+
+exports.getProjectsById = async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.id);
+
+        if(!project) {
+            return res.status(404).json({
+                success: false,
+                message: 'Project not found'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: project
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to fetch project',
+            error: error.message
+        });
+    }
+}
+
+exports.createProject = async (req, res) => {
+    try {
+        const project = Project.create(req.body);
+        res.status(201).json({
+            success: true,
+            message: 'Project created successfully',
+            data: project
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to create project',
+            error: error.message
+        });
+    }
+}
+
+exports.updateProject = async (req, res) => {
+    try {
+        const project = Project.findIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true, runValidators: true}
+        );
+
+        if(!project) {
+            return res.status(404).json({
+                success: false,
+                message: 'Project not found'
+            });
+        }
+
+        res.status(201).json({
+            success: true,
+            message: 'Project updated successfully',
+            data: project
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to update project',
+            error: error.message
+        });
+    }
+}
+
+exports.deleteProject = async (req, res) => {
+    try {
+        const project = Project.findByIdAndDelete(req.params.id);
+        
+        if(!project) {
+            return res.status(404).json({
+                success: false,
+                message: 'Project not found'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'Project deleted successfully',
+            data: project
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to delete project',
+            error: error.message
+        });
+    }
+}
